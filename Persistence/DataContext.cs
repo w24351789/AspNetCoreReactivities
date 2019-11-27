09d952c7fd1f9d2d711232edb11c046c5bae139a
intumit <w24351789@gmail.com>
2019-11-28 00:56:05 +0800

    Add DataContext and register service in Startup

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Domain;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Value> Values { get; set; }  //using Domain   here "Values" is be used to db table name 
+    }
+}
