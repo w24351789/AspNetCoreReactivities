43dc183b833cdbe63c4cca7231a252be0bbcfab7
intumit <w24351789@gmail.com>
2019-11-28 01:31:57 +0800

    Add SeedData
    dotnet ef migrations add SeedValues -p Persistence/ -s API/

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -12,5 +12,16 @@ namespace Persistence
         }
 
         public DbSet<Value> Values { get; set; }  //using Domain   here "Values" is be used to db table name 
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Value>()
+                .HasData(
+                    new Value { Id = 1, Name = "Value101"},
+                    new Value { Id = 2, Name = "Value102"},
+                    new Value { Id = 3, Name = "Value103"}
+                );
+        }
+        
     }
 }
