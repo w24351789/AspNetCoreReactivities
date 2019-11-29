9d2f58adf75aa14741196cb5a4b2a21a6de67565
TED <W24351789@GMAIL.COM>
2019-11-29 18:10:01 +0800

    Add Find details handler

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -25,5 +25,11 @@ namespace API.Controllers
         {
             return await _mediator.Send(new List.Query());
         }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> Details(Guid id)
+        {
+            return await _mediator.Send(new Details.Query { Id = id });
+        }
+
     }
 }
