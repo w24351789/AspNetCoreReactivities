87559cb95a5a1041e530ad8ccd25285b40dd0480
intumit <w24351789@gmail.com>
2019-12-01 00:18:20 +0800

    Add CRUD activities with mediator and api controller

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Application.Activities;
 using Domain;
 using MediatR;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
@@ -30,6 +28,22 @@ namespace API.Controllers
         {
             return await _mediator.Send(new Details.Query { Id = id });
         }
-
+        [HttpPost]
+        public async Task<ActionResult<Unit>> Command(Create.Command command)
+        {
+            return await _mediator.Send(command);
+        }
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Unit>> Edit(Guid id, Edit.Command command)
+        {
+            command.Id = id;
+            return await _mediator.Send(command);
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Unit>> Delete(Guid id)
+        {
+ 
+            return await _mediator.Send(new Delete.Command { Id = id});
+        }
     }
 }
