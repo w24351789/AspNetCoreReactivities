67261785401f0cdeec1b6fcc012f1ac62e6b212a
intumit <w24351789@gmail.com>
2019-12-05 02:11:35 +0800

    add api agent

---
+++ client-app/src/app/api/agent.ts
@@ -0,0 +1,31 @@
+import Axios, {AxiosResponse} from 'axios'
+import { IActivity } from '../models/activity'
+import { resolve } from 'dns'
+
+Axios.defaults.baseURL = 'http://localhost:5000/api'  //本agent的baseurl
+
+const responseBody = (response: AxiosResponse) => response.data
+
+const sleep = (ms: number) => (response: AxiosResponse) => 
+    new Promise<AxiosResponse>(resolve => setTimeout(() => resolve(response), ms)) 
+
+// 實作CRUD
+const request = {
+    get: (url: string) => Axios.get(url).then(sleep(1000)).then(responseBody),
+    post: (url: string, body: {}) => Axios.post(url, body).then(sleep(1000)).then(responseBody),
+    put: (url: string, body: {}) => Axios.put(url, body).then(sleep(1000)).then(responseBody),
+    del:　(url: string) => Axios.delete(url).then(sleep(1000)).then(responseBody)
+}
+
+// 用來獲取 activities api的物件
+const Activities = {
+    list: (): Promise<IActivity[]> => request.get('/activities'),
+    details: (id: string) => request.get(`/activities/${id}`),
+    create:　(activity :IActivity) => request.post('/activities', activity),
+    edit:　(activity: IActivity) => request.put(`/activities/${activity.id}`, activity),   //欲編輯之id直接從activity取
+    delete:　(id: string) => request.del(`/activities/${id}`)
+}
+
+export default {
+    Activities
+}
--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,45 +1,52 @@
 import React, {useState, useEffect, Fragment} from 'react';
 import { Container } from 'semantic-ui-react'
-import axios from 'axios';
 import { IActivity } from '../models/activity';
 import { NavBar } from '../../features/nav/NavBar';
 import { ActivityDashboard } from '../../features/activities/dashboard/ActivityDashboard';
+import agent from '../api/agent';
 
 
 const App = () => {
     const [activities, setActivities] = useState<IActivity[]>([])
     const [selectedActivity, setSelectedActivity] = useState<IActivity | null>(null)
-    const handleSelectedActivity = (id: string) => {
-      setSelectedActivity(activities.filter(a => a.id === id)[0])
-      setEditMode(false)
-    }
     const [editMode, setEditMode] = useState(false)
+    
     const handleCreateOpenForm = () => {
       setSelectedActivity(null)
       setEditMode(true)
     }
+    const handleSelectedActivity = (id: string) => {
+      setSelectedActivity(activities.filter(a => a.id === id)[0])
+      setEditMode(false)
+    }
 
     const handleCreateActivity = (activity: IActivity) => {
-      setActivities([...activities, activity])  //add activity into activities
-      setSelectedActivity(activity)
-      setEditMode(false)
+      agent.Activities.create(activity).then(() => {
+        setActivities([...activities, activity])  //add activity into activities
+        setSelectedActivity(activity)
+        setEditMode(false)
+      })
     }
 
     const handleEditActivity = (activity: IActivity) => {
-      setActivities([...activities.filter(a => a.id !== activity.id), activity])
-      setSelectedActivity(activity)
-      setEditMode(false)
+      agent.Activities.edit(activity).then(() => {
+        setActivities([...activities.filter(a => a.id !== activity.id), activity]) //讓除了要edit的activity維持原狀
+        setSelectedActivity(activity)
+        setEditMode(false)
+      })
     }
 
     const handleDeleteActivity = (id: string) => {
-      setActivities([...activities.filter(a => a.id !== id)])
+      agent.Activities.delete(id).then(() => {
+        setActivities([...activities.filter(a => a.id !== id)])
+      })
     }
 
     useEffect(() => {
-      axios.get<IActivity[]>('http://localhost:5000/api/activities')
+      agent.Activities.list()
         .then(response => {
           let activities: IActivity[] = []
-          response.data.forEach(activity => {
+          response.forEach((activity) => {
             activity.date = activity.date.split('.')[0]
             activities.push(activity)
           })
